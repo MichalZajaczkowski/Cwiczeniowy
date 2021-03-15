fd0974874579bec075a5d57591a4106c91fe9cf5
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-15 18:47:54 +0100

    zad2_wyliczanie ilosci ktopek w tekście

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println(textWithDots(text));
+
+    }
+
+    private static int textWithDots(String s) {
+        char[] chars = s.toCharArray();
+        int counter = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i] == '.'){
+                counter++;
+            }
+        }
+        return counter;
     }
 }
